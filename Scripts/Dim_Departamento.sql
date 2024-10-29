c73727b9901d2d922ff5623422b6b98792891be7
JulianoMata <jfmatta@gmail.com>
2024-10-29 10:03:47 -0300

    Remove scripts desnecessários da pasta Scripts

--- Scripts/Dim_Departamento.sql
+++
@@ -1,9 +0,0 @@
--- Seleciona o banco de dados ColegioDIO
-USE ColegioDIO;
-
--- Tabela Dim_Departamento
-CREATE TABLE Dim_Departamento (
-    ID_Departamento INT IDENTITY PRIMARY KEY,
-    Nome_Departamento VARCHAR(100),
-    Localizacao VARCHAR(100)
-);
